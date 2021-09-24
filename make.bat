465f69073ef335e4478a1c2347c46fb473bc3539
Abe Pralle <abe.pralle@gmail.com>
2021-09-23 22:01:20 -1000

    [Build] Added make.bat for Windows installation

---
+++ make.bat
@@ -0,0 +1,10 @@
+@if not exist Build mkdir Build
+
+roguec Source/Rogo.rogue --compile --compile-arg="/Ob2ity /GF /Gy" --output=Build\rogo
+
+@if "%~1" == "build" goto DONE
+@echo Add the following to Environment Variables ^> User variables ^> Path:
+@echo   %CD%\Build
+
+:DONE
+
