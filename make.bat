c755cfc0d5dc4af7209324d2280fe6fcb9a77c80
Abe Pralle <abe.pralle@gmail.com>
2021-09-24 21:06:03 -1000

    [Build] Commented out Win path instructions - against the grain of Morlock install

--- make.bat
+++ make.bat
@@ -2,5 +2,5 @@
 
 roguec Source/Rogo.rogue --compile --compile-arg="/Ob2ity /GF /Gy" --output=Build\rogo
 
-@echo Add the following to Environment Variables ^> User variables ^> Path:
-@echo %CD%\Build
+@rem echo Add the following to Environment Variables ^> User variables ^> Path:
+@rem echo %CD%\Build
