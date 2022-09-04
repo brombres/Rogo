6439e1db9b15c58f67db14a41a88d567971a54d5
Abe Pralle <abe.pralle@gmail.com>
2022-09-04 12:30:42 -1000

    [Windows] Fixed bugs in make.bat

--- make.bat
+++ make.bat
@@ -1,6 +1,7 @@
 @if not exist Build mkdir Build
 
-roguec Source/Rogo.rogue --compile --compile-arg="/Ob2ity /GF /Gy" --output=Build\rogo
+start /b roguec Source/Rogo.rogue --output=Build\Rogo-Windows
+cl /Ob2ity /GF /Gy /EHsc /nologo Build\Rogo-Windows.c /FoBuild\Rogo-Windows.obj /FeBuild\rogo.exe
 
 @if "%~1" == "build" goto DONE
 @echo Add the following to Environment Variables ^> User variables ^> Path:
