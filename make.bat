a810271d02fd4e189a732ddc7e2ad75ecea5cbe9
Abe Pralle <abe.pralle@gmail.com>
2022-09-05 18:01:42 -1000

    [Windows] Tweaks

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @if not exist Build mkdir Build
 
-start /b roguec Source/Rogo.rogue --output=Build\Rogo-Windows
+call roguec Source/Rogo.rogue --main --output=Build\Rogo-Windows
 cl /Ob2ity /GF /Gy /EHsc /nologo Build\Rogo-Windows.c /FoBuild\Rogo-Windows.obj /FeBuild\rogo.exe
 
 @if "%~1" == "build" goto DONE
