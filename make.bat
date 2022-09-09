7b72118c3e820a47d2619f09236656b7627dd94d
Abe Pralle <abe.pralle@gmail.com>
2022-09-09 09:57:50 -1000

    [Windows] Small improvement to make.bat

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @if not exist Build mkdir Build
 
 call roguec Source/Rogo.rogue --main --output=Build\Rogo-Windows
-cl /Ob2ity /GF /Gy /EHsc /nologo Build\Rogo-Windows.c /FoBuild\Rogo-Windows.obj /FeBuild\rogo.exe
+cl /O2 /nologo Build\Rogo-Windows.c /FoBuild\Rogo-Windows.obj /FeBuild\rogo.exe
 
 @if "%~1" == "build" goto DONE
 @echo Add the following to Environment Variables ^> User variables ^> Path:
